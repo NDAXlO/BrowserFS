85343feb8b12aca2cbc90bf950773b62a2ff69d8
Tom Wilberding <tom@wilberding.com>
2017-08-03 15:17:29 -0500

    Initial Fork
    Added proper handling for windows linebreaks
    Disabled backends that we do not intend to use

--- src/core/backends.ts
+++ src/core/backends.ts
@@ -1,10 +1,9 @@
-import AsyncMirror from '../backend/AsyncMirror';
+/*import AsyncMirror from '../backend/AsyncMirror';
 import Dropbox from '../backend/Dropbox';
 import Emscripten from '../backend/Emscripten';
 import FolderAdapter from '../backend/FolderAdapter';
 import HTML5FS from '../backend/HTML5FS';
 import InMemory from '../backend/InMemory';
-import IndexedDB from '../backend/IndexedDB';
 import LocalStorage from '../backend/LocalStorage';
 import MountableFileSystem from '../backend/MountableFileSystem';
 import OverlayFS from '../backend/OverlayFS';
@@ -12,10 +11,13 @@ import WorkerFS from '../backend/WorkerFS';
 import XmlHttpRequest from '../backend/XmlHttpRequest';
 import ZipFS from '../backend/ZipFS';
 import IsoFS from '../backend/IsoFS';
+*/
+import IndexedDB from '../backend/IndexedDB';
 /* tslint:disable:variable-name */
 /**
  * @hidden
  */
-const Backends = { AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, XmlHttpRequest, ZipFS };
+ // const Backends = { AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, XmlHttpRequest, ZipFS };
+const Backends = { IndexedDB };
 export default Backends;
 /* tslint:enable:variable-name */
